ea115f2105143ee56b7fd43bf1786937ae077967
kirna <kirna1851994@gmail.com>
2021-02-01 00:48:56 -0800

    Initial Commit

---
+++ A2_Fa_IOS_KIRNA_779568/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A2_Fa_IOS_KIRNA_779568
+//
+//  Created by user191875 on 2/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
